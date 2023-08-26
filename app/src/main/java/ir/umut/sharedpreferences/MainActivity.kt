3ca9fb20dafe5618cd5b8428ee48f7040b5c99f7
Omid Haqi <omid.haqi@outlook.com>
2023-08-26 18:12:29 +0330

    first commit

---
+++ app/src/main/java/ir/umut/sharedpreferences/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.umut.sharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
